f0b7351253c5fd9b050d6b07bc0fa45f34d7ecee
Elisag28 Gonzalez <elisagonzalez281296@gmail.com>
2023-11-02 16:54:52 +0000

    almost done titanic

--- src/titanic_.py
+++ src/titanic_.py
@@ -2,11 +2,27 @@ import psycopg2
 import sqlite3 as sq
 import pandas as pd
 
+#Responder a las siguientes preguntas usando SQL sobre la tabla que acabáis de crear:
+#1 Cuántos supervivientes hay (columna Survived)
+# 2 De todos los pasajeros, cuántos hombres y mujeres hay (columna Sex)
+# 3. Cuál es el valor del ticket más caro que se compró (columna Fare)
+
 df = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/machine-learning-content/master/assets/titanic_train.csv", sep = ',')
 
 
-conn = sq.connect('mi_base_de_datos.db')
+conn = sq.connect('mi_base_de_datos')
 
-df.to_sql('mi_base_de_datos.db', conn, if_exists='replace', index=False)
+cur = conn.cursor()
 
-conn.execute("""SELECT * FROM mi_base_de_datos.db""")
+df.to_sql('mi_base_de_datos', conn, if_exists='replace', index=False)
+
+surv = cur.execute('SELECT COUNT(Survived) FROM mi_base_de_datos WHERE Survived = 1 Group by Survived Order by COUNT(Survived) DESC')
+
+#print(f'Total survivors:',{surv.fetchone()})
+
+sex = cur.execute('SELECT COUNT(Sex) Sex FROM mi_base_de_datos Group by Sex Order by Sex')
+output = sex.fetchall()
+
+#print(*output)
+
+tick = cur.execute('SELECT COUNT(Sex) Sex FROM mi_base_de_datos Group by Sex Order by Sex')
